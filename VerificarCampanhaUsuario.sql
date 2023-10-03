2401d81803344ba66e60e7a25e23db272b86f352
MATEUS GUEDES <mateusguedes17@gmail.com>
2023-10-03 14:12:16 -0300

    Create VerificarCampanhaUsuario.sql

---
+++ VerificarCampanhaUsuario.sql
@@ -0,0 +1,8 @@
+SELECT campanha.Nome, campanha_usuario.*, usuario.email
+FROM campanha
+INNER JOIN campanha_usuario ON campanha.id = campanha_usuario.Campanha_id
+INNER JOIN usuario ON campanha_usuario.usuarios_id = usuario.id
+WHERE campanha_usuario.usuarios_id IN (
+'394   ',
+'1026  ',
+'66578 ');
