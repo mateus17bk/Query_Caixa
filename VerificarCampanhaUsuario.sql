a757b5378b28ee98a826fdadddbe26248d7da0ac
MATEUS GUEDES <mateusguedes17@gmail.com>
2024-08-23 16:24:42 -0300

    Update VerificarCampanhaUsuario.sql

--- VerificarCampanhaUsuario.sql
+++ VerificarCampanhaUsuario.sql
@@ -6,3 +6,13 @@ WHERE campanha_usuario.usuarios_id IN (
 '394   ',
 '1026  ',
 '66578 ');
+
+-------------------------------------
+
+
+SELECT campanha.Nome, campanha_usuario.*, usuario.nome
+FROM campanha
+INNER JOIN campanha_usuario ON campanha.id = campanha_usuario.Campanha_id
+INNER JOIN usuario ON campanha_usuario.usuarios_id = usuario.id
+WHERE campanha.Nome = '8888'
+AND usuario.id IN ()
