9d8ca254a733f348a7b3cb5e015e8179cf2cf3c3
MATEUS GUEDES <mateusguedes17@gmail.com>
2025-07-22 16:15:39 -0300

    Update AtendimentosVNA.sql

--- AtendimentosVNA.sql
+++ AtendimentosVNA.sql
@@ -1,15 +1,52 @@
 SELECT 
-   cq.DateAdded
-  ,( select TOP 1  CAST(cv1.Value AS VARCHAR(MAX))  from   History_Chat.dbo.chatqueue_custom_variables cv1 where  cv1.ChatQueue_Sequence_ID = cq.Sequence_ID and cv1.Name = '@CpfCnpj'   ) as cpf
-   ,  cq.Tel_Client_ID  as telefone
-   ,( select   CAST(cv1.Value AS VARCHAR(MAX)) from   History_Chat.dbo.chatqueue_custom_variables cv1 where  cv1.ChatQueue_Sequence_ID = cq.Sequence_ID and cv1.Name = '@DataVencimento' ) as vencimento
-   ,( select   CAST(cv1.Value AS VARCHAR(MAX)) from   History_Chat.dbo.chatqueue_custom_variables cv1 where  cv1.ChatQueue_Sequence_ID = cq.Sequence_ID and cv1.Name = '@ContratosVNA' ) as contratoVNA 
-    ,( select   CAST(cv1.Value AS VARCHAR(MAX)) from   History_Chat.dbo.chatqueue_custom_variables cv1 where  cv1.ChatQueue_Sequence_ID = cq.Sequence_ID and cv1.Name = '@ValorBoleto' ) as valor 
-        ,( select   CAST(cv1.Value AS VARCHAR(MAX)) from   History_Chat.dbo.chatqueue_custom_variables cv1 where  cv1.ChatQueue_Sequence_ID = cq.Sequence_ID and cv1.Name = '@LinhaDigitavel' ) as LinhaDigitavel 
-                ,( select   CAST(cv1.Value AS VARCHAR(MAX)) from   History_Chat.dbo.chatqueue_custom_variables cv1 where  cv1.ChatQueue_Sequence_ID = cq.Sequence_ID and cv1.Name = '@RetornoIntegracao' ) as RetornoIntegracao 
-FROM    History_Chat.dbo.chatqueue cq WITH (NOLOCK, INDEX(IX_chatqueue_DateAdded))
-  INNER JOIN  History_Chat.dbo.chatqueue_custom_variables cv WITH (NOLOCK)
-   ON cv.ChatQueue_Sequence_ID = cq.Sequence_ID
+  cq.Tel_Client_ID AS telefone,
+
+  (
+    SELECT CAST(cv1.Value AS CHAR)
+    FROM History_Chat.chatqueue_custom_variables cv1
+    WHERE cv1.ChatQueue_Sequence_ID = cq.Sequence_ID AND cv1.Name = '@CpfCnpj'
+    LIMIT 1
+  ) AS cpf,
+
+  (
+    SELECT CAST(cv1.Value AS CHAR)
+    FROM History_Chat.chatqueue_custom_variables cv1
+    WHERE cv1.ChatQueue_Sequence_ID = cq.Sequence_ID AND cv1.Name = '@ContratosVNA'
+    LIMIT 1
+  ) AS contratoVNA,
+
+  (
+    SELECT CAST(cv1.Value AS CHAR)
+    FROM History_Chat.chatqueue_custom_variables cv1
+    WHERE cv1.ChatQueue_Sequence_ID = cq.Sequence_ID AND cv1.Name = '@ValorBoleto'
+    LIMIT 1
+  ) AS valor,
+
+  (
+    SELECT CAST(cv1.Value AS CHAR)
+    FROM History_Chat.chatqueue_custom_variables cv1
+    WHERE cv1.ChatQueue_Sequence_ID = cq.Sequence_ID AND cv1.Name = '@DataVencimento'
+    LIMIT 1
+  ) AS vencimento,
+
+  DATE_FORMAT(cq.DateAdded, "%d/%m/%Y") AS data_aceitacao,
+
+  DATE_FORMAT(cq.DateAdded, "%H:%i:%s") AS hora_aceitacao,
+
+  (
+    SELECT CAST(cv1.Value AS CHAR)
+    FROM History_Chat.chatqueue_custom_variables cv1
+    WHERE cv1.ChatQueue_Sequence_ID = cq.Sequence_ID AND cv1.Name = '@LinhaDigitavel'
+    LIMIT 1
+  ) AS linha_digitavel,
+
+  cq.DateAdded
+
+FROM History_Chat.chatqueue cq
+INNER JOIN History_Chat.chatqueue_custom_variables cv
+  ON cv.ChatQueue_Sequence_ID = cq.Sequence_ID
 WHERE
-  cq.DateAdded >= '2023-09-20 00:00:00' AND cq.DateAdded  <= '2023-09-25 23:59:59'  AND cv.Name IN ('@ContratosVNA')
-ORDER BY  cq.DateAdded, vencimento
+  cq.DateAdded >= '2025-07-11 00:00:00'
+  AND cq.DateAdded <= '2025-07-11 23:59:59'
+  AND cv.Name IN ('@ContratosVNA')
+ORDER BY cq.DateAdded, vencimento;
