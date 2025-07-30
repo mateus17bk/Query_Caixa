2583aacd492a9e5bd33f655c4d22a325b8abfeb6
MATEUS GUEDES <mateusguedes17@gmail.com>
2025-07-30 16:41:33 -0300

    Update Caminha_ParaUnidade.js

--- Caminha_ParaUnidade.js
+++ Caminha_ParaUnidade.js
@@ -9,3 +9,26 @@ db.chatQueue.updateMany(
   ] } },
   { $set: { QueueId: "7777" } }
 )
+
+----------------------------------------------------------------
+
+  db.chatQueue.updateMany(
+  {
+    QeueId: "TESTE_ITX",
+    Active: true,
+    Finished: false,
+    ForceClose: false,
+    DateAdded: {
+      $gte: ISODate("2025-07-29T00:00:00.000Z"),
+      $lt: ISODate("2025-07-29T23:59:59.000Z")
+    }
+  },
+  {
+    $set: {
+      Active: false,
+      Finished: true,
+      ForceClose: true,
+      DateFinished: ISODate("2025-07-30T16:40:00.000Z")
+    }
+  }
+)
